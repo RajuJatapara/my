8a1980c4aeb188a92d8849ae68df26d65d8e8ae6
Rajesh <rajeshjjatapara@gmail.com>
2026-08-02 15:20:16 +0530

    feat: enable AdSense, update publisher ID, and migrate app domain to Netlify

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
-window.APP_SHARE_DOMAIN = "https://rajujatapara.github.io/my/";
-window.ENABLE_ADSENSE = false; // Set to false to disable all ads instantly!
-window.ADSENSE_PUB_ID = "ca-pub-XXXXXXXXXXXXXXXX"; // Your AdSense Publisher ID
+window.APP_SHARE_DOMAIN = "https://rajutools.netlify.app/";
+window.ENABLE_ADSENSE = true; // Set to false to disable all ads instantly!
+window.ADSENSE_PUB_ID = "ca-pub-2104663140030820"; // Your AdSense Publisher ID
 window.SUPPORT_PHONE = "916356239260"; // Centralized WhatsApp & Call Helpline Phone Number (with Country Code, no spaces/plus)
 window.CREATOR_NAME = "Raju Jatapara"; // Founder/Creator Name
 
